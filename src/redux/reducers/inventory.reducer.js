4548b29f7382083719147d980a6a7f542cc26cca
Krys Frost <librarykrystal@gmail.com>
2023-02-25 22:36:35 -0600

    Added inventory reducer and saga. Added inventory route to server.js.

---
+++ src/redux/reducers/inventory.reducer.js
@@ -0,0 +1,12 @@
+const inventoryReducer = (state = {}, action) => {
+    switch (action.type) {
+      case 'SET_INVENTORY':
+        return action.payload;
+    //   case 'CLEAR_INVENTORY':
+    //     return {};
+      default:
+        return state;
+    }
+  };
+  
+  export default inventoryReducer;
