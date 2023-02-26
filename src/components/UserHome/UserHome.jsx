0998e4ea968fa1f82bb1963a3c04775dbf6672e7
Krys Frost <librarykrystal@gmail.com>
2023-02-25 22:47:04 -0600

    UserHome: useEffect dispatch gets user's inventory from store.

--- src/components/UserHome/UserHome.jsx
+++ src/components/UserHome/UserHome.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import {useHistory} from 'react-router-dom';
 
   // this component will show MAIN INVENTORY LIST
 
 function UserHome() {
+
   const user = useSelector((store) => store.user);
+  const inventory = useSelector(store => store.inventory);
+  const dispatch = useDispatch();
+  // const history = useHistory();
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_USER_INVENTORY' });
+  }, []);
+
+
+
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
       <p>This is where your inventory list will be!</p>
 
-
+      <p>DATA TEST: {JSON.stringify(inventory)}</p>
 
 
       {/* <LogOutButton className="btn" /> */}
