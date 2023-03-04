ca8e943169e6bcceca43356726da73953ebdff5c
Krys Frost <librarykrystal@gmail.com>
2023-03-04 16:43:11 -0600

    Custom logo added to Nav.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,17 +3,22 @@ import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import { useSelector } from 'react-redux';
+import logo from './logo.png';
+
 
 function Nav() {
   const user = useSelector((store) => store.user);
+
+  // console.log('logo:', logo);
 
   return (
     <div className="navOuterContainer">
     <div className="nav">
       <Link to="/home">
-        <h2 className="nav-title">THE RUNDOWN</h2>
+        {/* <h2 className="nav-title">THE RUNDOWN</h2> */}
+        <img className="logoImage" src={logo} alt='logo'/>
       </Link>
-      <h4 className="subtitle">Artist's Paint Inventory</h4>
+      {/* <h4 className="subtitle">Artist's Paint Inventory</h4> */}
       <div>
         {/* If no user is logged in, show these links */}
         {!user.id && (
