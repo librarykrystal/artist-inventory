d748bc1ebef47d7c4924957bac47d27d7549779b
Krys Frost <librarykrystal@gmail.com>
2023-03-11 00:54:08 -0600

    Styling and Cabin font finished on LoginForm and LoginPage.

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -2,6 +2,33 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import {useSelector} from 'react-redux';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import grey from '@mui/material/colors/grey';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import '@fontsource/cabin/400.css';
+import '@fontsource/cabin/700.css';
+import TextField from '@mui/material/TextField';
+import InfoIcon from '@mui/icons-material/Info';
+import LoginIcon from '@mui/icons-material/Login';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Cabin',
+    ],
+  },
+  palette: {
+    primary: {
+      main: '#d9d9d9',
+    },
+    secondary: {
+      main: grey[700],
+      contrastText: "#fff",
+    },
+  },
+});
+
 function LoginForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -25,15 +52,19 @@ function LoginForm() {
   }; // end login
 
   return (
-    <form className="formPanel" onSubmit={login}>
-      <h2>Login</h2>
+    <ThemeProvider theme={theme}>
+    {/* <form className="formPanel" onSubmit={login}> */}
+    <div className="registerForm">
+    <Typography variant="h4" mt={0} mb={1} gutterBottom>Log In</Typography>
+      {/* <h2>Login</h2> */}
       {errors.loginMessage && (
         <h3 className="alert" role="alert">
           {errors.loginMessage}
         </h3>
       )}
+
       <div>
-        <label htmlFor="username">
+        {/* <label htmlFor="username">
           Username:
           <input
             type="text"
@@ -42,10 +73,14 @@ function LoginForm() {
             value={username}
             onChange={(event) => setUsername(event.target.value)}
           />
-        </label>
+        </label> */}
+      
+
+      <TextField id="username" required label="Username" variant="standard" value={username} onChange={(e) => setUsername(e.target.value)} />
       </div>
+
       <div>
-        <label htmlFor="password">
+        {/* <label htmlFor="password">
           Password:
           <input
             type="password"
@@ -54,12 +89,28 @@ function LoginForm() {
             value={password}
             onChange={(event) => setPassword(event.target.value)}
           />
-        </label>
+        </label> */}
+
+        <TextField id="password" required label="Password" variant="standard" value={password} onChange={(e) => setPassword(e.target.value)} />
       </div>
-      <div>
+
+      {/* <div>
         <input className="btn" type="submit" name="submit" value="Log In" />
-      </div>
-    </form>
+      </div> */}
+
+      <br />
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        size="large"
+        startIcon={<LoginIcon />}
+        onClick={login}>SUBMIT
+      </Button>
+
+    {/* </form> */}
+    </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -2,25 +2,66 @@ import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import grey from '@mui/material/colors/grey';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import '@fontsource/cabin/400.css';
+import '@fontsource/cabin/700.css';
+import TextField from '@mui/material/TextField';
+import InfoIcon from '@mui/icons-material/Info';
+import LoginIcon from '@mui/icons-material/Login';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Cabin',
+    ],
+  },
+  palette: {
+    primary: {
+      main: '#d9d9d9',
+    },
+    secondary: {
+      main: grey[700],
+      contrastText: "#fff",
+    },
+  },
+});
+
 function LoginPage() {
   const history = useHistory();
 
   return (
-    <div className="loginContainer">
-      <LoginForm />
+    <ThemeProvider theme={theme}>
+      <div className="regContainer">
+        <LoginForm />
 
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/registration');
-          }}
-        >
-          Register
-        </button>
-      </center>
-    </div>
+        <center>
+          {/* <button
+            type="button"
+            className="btn btn_asLink"
+            onClick={() => {
+              history.push('/registration');
+            }}
+          >
+            Register
+          </button> */}
+
+<Typography variant="h6" mt={3} mb={1} gutterBottom>Not registered?</Typography>
+
+<Button
+  variant="contained"
+  color="secondary"
+  size="large"
+  // startIcon={<LoginIcon />}
+  onClick={() => {
+    history.push('/registration');
+  }}>REGISTER
+</Button>
+        </center>
+      </div>
+    </ThemeProvider>
   );
 }
 
